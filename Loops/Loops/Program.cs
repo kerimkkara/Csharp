95394f6a6180c19f4e4bea80d549d1582ddbf879
Kerim Kara <kara.kerim@outlook.com>
2023-03-08 18:04:37 +0300

    LoopsDemo

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int n = 0;
+while (true)
+{
+    n++;
+    if (n == 10)
+    {
+        break;
+    }
+}
